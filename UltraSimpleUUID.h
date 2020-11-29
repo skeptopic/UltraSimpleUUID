51541e0dac30fc9bcaff67bfedd47552c1afe556
Neil Chatterjee <neil.chatterjee@random.studio>
2020-11-30 00:18:21 +0100

    WIP

--- UltraSimpleUUID.h
+++ UltraSimpleUUID.h
@@ -6,31 +6,92 @@
 // If you want better randomness, check out  https://github.com/mariusbancila/stduuid, but I had problems building this on Linux.
 //
 #pragma once
+
+#define DEBUG_UUIDS
+
 #include <string>
 #include <cstdlib>
 #include <random>
 #include <iostream>
+#include <set>
+#ifdef DEBUG_UUIDS
+#include <cassert>
+#endif //DEBUG_UUIDS
 
-namespace SimpleUUID {
+namespace UltraSimpleUUID {
     const std::string CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    const std::set<int> HYPHEN_POSITIONS {8, 13, 18, 23};
+    const unsigned short UUID_NUM_CHARS = 32;
+    const unsigned short UUID_STR_LEN = UUID_NUM_CHARS + HYPHEN_POSITIONS.size();
 
-    std::string generateUUID() {
-        std::string uuid = std::string(36, ' ');
+    struct uuid 
+    {
+    public:
+        int32_t values[6] = { 0,0,0,0,0,0 };
 
-        std::random_device rd;
-        std::mt19937 mt(rd());
-        std::uniform_real_distribution<double> dist(0, CHARS.size());
-        std::vector<double> randoms;
-        for (int i = 0; i < uuid.size(); ++i)
-            randoms.push_back(dist(mt));
-
-        for (int i = 0; i < uuid.size(); i++)
-        {
-            if (i == 8 || i == 13 || i == 18 || i == 23) // Sequence of 8-4-4-4-12
-                uuid[i] = '-';
-            else
-                uuid[i] = CHARS[floor(randoms[i])];
+        std::string toString() {
+            std::string uuid = std::string(UUID_STR_LEN, '-');
+            int i = 0;
+            for (int n = 0; n < UUID_NUM_CHARS; n++)
+            {
+                if (HYPHEN_POSITIONS.find(n) != HYPHEN_POSITIONS.end())
+                    i++;
+                uuid[i] = CHARS[extractNumberAtPosition(n)];
+                i++;
+            }
+            return uuid;
         }
-        return uuid;
+
+
+
+        void randomize() {
+            std::random_device rd;
+            std::mt19937 mt(rd());
+            std::uniform_real_distribution<double> dist(0, CHARS.size());
+            std::vector<int32_t> randoms;
+            for (int i = 0; i < UUID_NUM_CHARS; i++)
+                randoms.push_back(floor(dist(mt)));
+
+            for (int num_idx = 0; num_idx < UUID_NUM_CHARS; num_idx++)
+            {
+#ifdef DEBUG_UUIDS
+                assert((randoms[num_idx] & 63) == randoms[num_idx]);
+#endif// DEBUG_UUIDS
+                encodeIntAtPosition(randoms[num_idx], num_idx);
+            };
+#ifdef DEBUG_UUIDS
+            for (int num_idx = 0; num_idx < UUID_NUM_CHARS; num_idx++)
+            {
+                assert(randoms[num_idx] == extractNumberAtPosition(num_idx));
+            }
+#endif //DEBUG_UUIDS
+        }
+
+    private:
+        void encodeIntAtPosition(int32_t inNumber, int inPosition)
+        {
+            for (int bit_idx = 0; bit_idx < 6; bit_idx++)
+            {
+                values[bit_idx] &= ~(1 << inPosition); // Make sure the bit is zero
+                values[bit_idx] |= ((inNumber >> bit_idx) & 1) << inPosition;
+            }
+        }
+
+        int32_t extractNumberAtPosition(int inPosition)
+        {
+            assert(inPosition < UUID_NUM_CHARS);
+            int32_t out_value = 0;
+            for (int bit_idx = 0; bit_idx < 6; bit_idx++)
+            {
+                out_value |= (((values[bit_idx] >> inPosition) & 1) << bit_idx);
+            }
+            return out_value;
+        }
+    };
+
+    std::string generate() {
+        uuid throwaway_uuid = uuid();
+        throwaway_uuid.randomize();
+        return throwaway_uuid.toString();
     }
 }
